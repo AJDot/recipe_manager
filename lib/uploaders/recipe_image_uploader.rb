27e8977f95cea6597648b6ca294048ebe670e447
Alex Henegar <alex.henegar19@gmail.com>
2020-11-22 14:12:27 -0500

    chore: do not store images with fog if testing

--- lib/uploaders/recipe_image_uploader.rb
+++ lib/uploaders/recipe_image_uploader.rb
@@ -1,7 +1,7 @@
 require 'carrierwave'
 
 class RecipeImageUploader < CarrierWave::Uploader::Base
-  storage :fog
+  storage :fog unless Sinatra::Application.test?
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
